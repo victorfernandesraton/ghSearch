f45dd7908a66fa59d8e9b3f00011111ec46621a9
victorfernandesraton <vfbrato@gmail.com>
2023-01-14 13:01:29 -0300

    feat: implement basic input

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import SearchInput from './components/SearchInput'
 import "./theme.scss"
+import styles from "./App.module.scss"
 function App() {
 
   return (
     <div className="App">
+      <main>
+       <div className={styles.container}>
         <SearchInput />
+       </div>
+      </main>
     </div>
   )
 }
--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -2,7 +2,10 @@ import styles from "./style.module.scss"
 
 function SearchInput() {
   return (
-    <input type={"text"} className={styles.search} />
+    <div className={styles.container}>
+      <input type={"text"} className={styles.inpt} />
+      <button className={styles.btn}>Search</button>
+    </div>
   )
 }
 
