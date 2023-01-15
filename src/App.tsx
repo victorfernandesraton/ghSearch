2de2e3f21aad2bbc688fda73289436175912cdf7
victorfernandesraton <vfbrato@gmail.com>
2023-01-15 02:11:39 -0300

    feat: apollo graphQL implementation

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,46 @@ import styles from "./App.module.scss"
 import UserInfo from './components/userInfo'
 import data from "./mock.json"
 import RepositoryItem from './components/repositoryItem'
+import { useLazyQuery } from '@apollo/client'
+import { USER_QUERY } from './query/user'
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useDebounce } from './hooks/debounce'
 function App() {
+
+	const [query, setQuery] = useState<string>();
+
+	const onChangeText = useCallback((text: string = "") => {
+		setQuery(text);
+	}, [])
+
+	const debounceText = useDebounce(query, 500)
+
+	const [fetchUserInfoQuery, { data: userInfoData, loading: loadingUserInfo, error, called }] = useLazyQuery(USER_QUERY)
+
+	const userInfo = useMemo(() => userInfoData?.search?.nodes?.[0], [userInfoData])
+
+	useEffect(() => {
+		if (query?.length > 2 && !loadingUserInfo && debounceText)
+			fetchUserInfoQuery({
+				variables: {
+					username: query
+				}
+			})
+	}, [query])
 
 	return (
 		<div className="App">
 			<main>
 				<div className={styles.container}>
-					<SearchInput />
-					<UserInfo {...{ ...data.data.search.nodes[0] }} />
+					<SearchInput
+						onChange={onChangeText}
+					/>
+					{called && userInfo && (
+						<UserInfo
+							loading={loadingUserInfo}
+							{...{ ...userInfo }}
+						/>
+					)}
 					{data.data.search.nodes[0].repositories.edges.map(item =>
 						<RepositoryItem key={item.node.id} {...{ ...item.node }} />
 					)}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+
 import App from "./App";
+
 import "./index.scss";
+
+const {VITE_GITHUB_TOKEN} = import.meta.env
+
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+
+	const token = VITE_GITHUB_TOKEN 
+	console.log(token);
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ?`Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
+		<ApolloProvider client={client}>
 		<App />
+		</ApolloProvider>
 	</React.StrictMode>
 );
