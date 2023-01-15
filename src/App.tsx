9c6dd4e2a2aec2ede11ac9869847483e5391e92c
victorfernandesraton <vfbrato@gmail.com>
2023-01-15 14:57:55 -0300

    feat: content optmization

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,7 @@ function App() {
 				<meta charSet="UTF-8" />
 				<link rel="icon" type="image/svg+xml" href="/icon.svg" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="description" content="Search about github users and repositories" />
 			</Helmet>
 			<main>
 				<div className={styles.container}>
